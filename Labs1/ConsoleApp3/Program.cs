832770101f21539aefddae87a4ae77c22173947f
vienTM <thanminhvien@gmail.com>
2018-11-13 16:51:23 +0700

    cmm

---
+++ Labs1/ConsoleApp3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class SecondMax
+    {
+        private int firstNum;
+        private int secondNum;
+        private int thirdNum;
+        
+        // Constructor method
+        public SecondMax(int tempFirstNum, int tempSecondNum, int tempThirdNum)
+        {
+            firstNum = tempFirstNum;
+            secondNum = tempSecondNum;
+            thirdNum = tempThirdNum;
+        }
+
+        // 
+        public void DisplaySecondMax()
+        {
+           
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
